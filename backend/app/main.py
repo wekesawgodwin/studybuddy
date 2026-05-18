b5f6bbf75d65dd30df5048c0a536a05341da0be7
wekesawgodwin <wegowabz1@gmail.com>
2026-05-18 15:00:30 +0300

    fix: update CORS with a cors variable in the environment

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,10 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+origins = os.environ.get("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.info(f"PORT environment variable is: {os.environ.get('PORT', 'NOT SET')}")
@@ -11,10 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://frontend-production-184f5.up.railway.app/",  # ← replace with your actual frontend URL
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
